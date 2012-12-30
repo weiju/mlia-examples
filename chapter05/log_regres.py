170946aa5368590a3875620bc6782f40f72ab5be
Wei-ju Wu <weiju.wu@gmail.com>
2012-12-30 08:35:15 -0800

    added and tested plot_best_fit() function

--- chapter05/log_regres.py
+++ chapter05/log_regres.py
@@ -13,3 +13,50 @@ def load_dataset():
 
 def sigmoid(in_x):
     return 1.0 / (1 + exp(-in_x))
+
+def grad_ascent(data_mat_in, class_labels):
+    data_matrix = mat(data_mat_in)
+    label_mat = mat(class_labels).transpose()
+    m, n = shape(data_matrix)
+    alpha = 0.001
+    max_cycles = 500
+    weights = ones((n, 1))
+    for k in range(max_cycles):
+        h = sigmoid(data_matrix * weights)
+        error = (label_mat - h)
+        weights = weights + alpha * data_matrix.transpose() * error
+    return weights
+
+def plot_best_fit(weights):
+    """Note that the book has an error here (see Errata)
+    the weights parameter should be a numpy array, which
+    can be obtained by calling getA() on the grad_ascent()
+    function result
+    """
+    import matplotlib.pyplot as plt
+    data_mat, label_mat = load_dataset()
+    data_arr = array(data_mat)
+    n = shape(data_arr)[0]
+    xcoord1 = []
+    ycoord1 = []
+    xcoord2 = []
+    ycoord2 = []
+
+    for i in range(n):
+        if int(label_mat[i]) == 1:
+            xcoord1.append(data_arr[i, 1])
+            ycoord1.append(data_arr[i, 2])
+        else:
+            xcoord2.append(data_arr[i, 1])
+            ycoord2.append(data_arr[i, 2])
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcoord1, ycoord1, s=30, c='red', marker='s')
+    ax.scatter(xcoord2, ycoord2, s=30, c='green')
+    x = arange(-3.0, 3.0, 0.1)
+    y = (-weights[0] - weights[1] * x) / weights[2]
+    ax.plot(x, y)
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.show()
